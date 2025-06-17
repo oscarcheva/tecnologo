40985a2182bf2d9825539f9e9b01eb0db2c3c3f9
Oscar Echeverria <oecheverria22@gmail.com>
2025-06-17 17:05:55 -0300

    Almost all queries done, remaining sorting algorithm, bets during play and 3 query

---
+++ cpp/help.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World from c++ \n");
+
+    int a = 54;
+
+    printf("%d \n" , a/10);
+    printf("%d \n", a%10);
+
+    return 0;
+
+}
--- obligatorio_principios_programacion_java/src/Main.java
+++ obligatorio_principios_programacion_java/src/Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
@@ -17,56 +14,89 @@ public class Main {
 
     public static void main(String[] args) {
 
+        testData();
+        int choice = 0;
 
-        System.out.println("Welcome to the game menu, please chose the option suits you best");
-        System.out.println("1 - Player management");
-        System.out.println("2 - Game queries");
-        System.out.println("3 - Play the game");
-        System.out.println("4 - Exit");
+        while (choice != 4) {
+            System.out.println("Welcome to the game menu, please chose the option suits you best");
+            System.out.println("1 - Player management");
+            System.out.println("2 - Game queries");
+            System.out.println("3 - Play the game");
+            System.out.println("4 - Exit");
 
-        switch (sc.nextInt()) {
-            case 1 -> playerManagement();
-            case 2 -> gameQueries();
-            case 3 -> playGame();
-            case 4 -> System.out.println("Good bye!");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> playerManagement();
+                case 2 -> gameQueries();
+                case 3 -> playGame();
+                case 4 -> System.out.println("Good bye!");
+            }
         }
+    }
 
+    public static void testData() {
+
+        players.add(new Player(63333333, "Carleto", "Carlos", "Ramírez", true, 1000));
+        players.add(new Player(52222222, "Marita", "María", "Gómez", true, 1000));
+        players.add(new Player(41111111, "Juanca", "Juan Carlos", "Fernández", false, 1000));
+
+        bets.add(new Bet(150, false, "Carleto", 850));
+        bets.add(new Bet(150, true, "Marita", 1150));
+        bets.add(new Bet(300, false, "Carleto", 550));
+        bets.add(new Bet(550, true, "Carleto", 1100));
+        bets.add(new Bet(100, false, "Carleto", 1000));
+        bets.add(new Bet(700, false, "Juanca", 1000));
     }
 
     public static void playerManagement() {
-        System.out.println("Welcome to the player management menu, please chose the option suits you best");
-        System.out.println("1 - Create Player");
-        System.out.println("2 - Update Player");
-        System.out.println("3 - Delete Player");
-        System.out.println("4 - Back to previous menu");
+        int choice = 0;
 
-        switch (sc.nextInt()) {
-            case 1 -> createPlayer();
-            case 2 -> updatePlayer();
-            case 3 -> deletePlayer();
-            case 4 -> System.out.println("Going back...");
+        while (choice != 4) {
+            System.out.println("Welcome to the player management menu, please choose the option that suits you best:");
+            System.out.println("1 - Create Player");
+            System.out.println("2 - Update Player");
+            System.out.println("3 - Delete Player");
+            System.out.println("4 - Back to previous menu");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> createPlayer();
+                case 2 -> updatePlayer();
+                case 3 -> deletePlayer();
+                case 4 -> System.out.println("Going back...");
+                default -> System.out.println("❌ Invalid option. Please try again.");
+            }
         }
     }
 
+
     public static void createPlayer() {
-        Player DTOPlayer;
 
         System.out.println("Please enter the player's nickname");
 
-        DTOPlayer = checkExistingNickname(sc.next());
-        if (DTOPlayer != null) {
+        Player checkPlayer = getPlayer(sc.next());
+        if (checkPlayer != null) {
             System.out.println("A player already exists with the provided nickname");
-            if (!DTOPlayer.isActive())
+            if (!checkPlayer.isActive())
                 System.out.println("The player has been reactivated");
+
+            players.stream()
+                    .filter(player -> player.getNickName()
+                            .equalsIgnoreCase(checkPlayer.getNickName()))
+                    .findFirst()
+                    .ifPresent(player -> player.setActive(true));
             return;
         }
-        DTOPlayer = new Player();
+
+        Player DTOPlayer = new Player();
         DTOPlayer.setNickName(sc.next());
 
         System.out.println("Please enter the player's id");
         DTOPlayer.setId(sc.nextLong());
 
-        if (checkExistingID(DTOPlayer.getId())) {
+        if (checkValidID(DTOPlayer.getId())) {
             System.out.println("The ID provided is not valid");
             return;
         }
@@ -87,7 +117,7 @@ public class Main {
 
     }
 
-    public static Player checkExistingNickname(String nickName) {
+    public static Player getPlayer(String nickName) {
 
         return players.stream()
                 .filter(p -> p.getNickName().equalsIgnoreCase(nickName))
@@ -95,26 +125,114 @@ public class Main {
                 .orElse(null);
     }
 
-    public static boolean checkExistingID(long id) {
+    public static boolean checkValidID(long id) {
         return id < 1111111 || id > 99999999;
     }
 
     public static void updatePlayer() {
 
+        System.out.println("Please enter the player's nickname");
+
+        Player updatedPlayer = getPlayer(sc.next());
+        if (updatedPlayer == null) {
+            System.out.println("A player does not exists with that nickName");
+            return;
+        }
+        System.out.printf("Current first name is: %s \n", updatedPlayer.getFirstName());
+        System.out.println("Please enter the new first name");
+        updatedPlayer.setFirstName(sc.next());
+
+        System.out.printf("Current lastname name is: %s \n", updatedPlayer.getLastName());
+        System.out.println("Please enter the new last name");
+        updatedPlayer.setLastName(sc.next());
+
+        System.out.printf("Current id is: %d \n", updatedPlayer.getId());
+        System.out.println("Please enter the id name");
+        updatedPlayer.setId(sc.nextLong());
+
+        players.stream()
+                .filter(player -> player.getNickName()
+                        .equalsIgnoreCase(updatedPlayer.getNickName()))
+                .findFirst()
+                .ifPresent(player -> {
+                    player.setFirstName(updatedPlayer.getFirstName());
+                    player.setLastName(updatedPlayer.getLastName());
+                    player.setId(updatedPlayer.getId());
+                });
+
+        System.out.println("Player updated successfully");
     }
 
     public static void deletePlayer() {
 
+        System.out.println("Please enter the player's nickname");
+
+        Player removedPlayer = getPlayer(sc.next());
+        if (removedPlayer == null) {
+            System.out.println("A player does not exists with that nickName");
+            return;
+        }
+        players.stream()
+                .filter(player -> player.getNickName()
+                        .equalsIgnoreCase(removedPlayer.getNickName()))
+                .findFirst()
+                .ifPresent(player -> player.setActive(false));
+        System.out.println("Player removed successfully");
     }
 
     public static void gameQueries() {
+        int choice = 0;
+
+        while (choice != 4) {
+            System.out.println("Welcome to the queries menu, please choose the option that suits you best:");
+            System.out.println("1 - Display active Players");
+            System.out.println("2 - Display all bets");
+            System.out.println("3 - Display bets by player");
+            System.out.println("4 - Back to previous menu");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> displayAllPlayers();
+                case 2 -> displayAllBets();
+                case 3 -> deletePlayer();
+                case 4 -> System.out.println("Going back...");
+                default -> System.out.println("❌ Invalid option. Please try again.");
+            }
+        }
+    }
+
+    public static void displayAllPlayers() {
+        System.out.printf("%-15s %-15s %-15s %-15s %n", "Nickname", "First Name", "Last Name", "Balance");
+
+        for (Player player : players) {
+            if (player.isActive())
+                System.out.printf("%-15s %-15s %-15s %-15s %n", player.getNickName(), player.getFirstName(), player.getLastName(), player.getBalance());
+        }
+    }
+
+    public static void displayAllBets() {
+        System.out.printf("%-15s %-17s %-15s %-20s%n", "Nickname", "Amount Gambled", "Won?", "Remaining Balance");
+
+        for (Bet bet : bets) {
+            System.out.printf(
+                    "%-15s %-17d %-15s %-20d%n",
+                    bet.getNickName(),
+                    bet.getBetAmount(),
+                    bet.isOutcome() ? "Player won" : "Player lost",
+                    bet.getRemainingBalance()
+            );
+        }
+    }
+
+    public static void displayBetsByPlayers() {
 
     }
 
     private static void playGame() {
         int balance;
         int bet;
-        int choice ;
+        int choice;
         int outcome;
         boolean hit;
 
@@ -129,7 +247,7 @@ public class Main {
         }
         choice = createCups();
 
-        while (choice != (GAME_SIZE + 1) && balance >= MINIMAL_BET) {
+        while (choice != (GAME_SIZE + 1)) {
             outcome = random.nextInt(3) + 1;
 
             bet = placeBet();
